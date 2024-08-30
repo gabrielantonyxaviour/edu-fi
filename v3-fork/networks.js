39de925265d111c6e55ee73417537284ef12aa66
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-08-31 00:15:54 +0530

    create pool tested

--- v3-fork/networks.js
+++ v3-fork/networks.js
@@ -22,6 +22,12 @@ const networks = {
     url: "http://127.0.0.1:8545/",
     chainId: 31337,
     confirmations: 1,
+    usdt: "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9",
+    link: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
+    usdc: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+    dai: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    weth: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
+    poolFactory: "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707",
   },
   arbitrumSepolia: {
     gasPrice: undefined,
@@ -37,7 +43,7 @@ const networks = {
     usdc: "0x5030Eed1EC6f1205CD9317Ac2a229A2cEecE24ed",
     dai: "0x9050d7A160423d50F5B7C9eD3A0e44866Da7836b",
     weth: "0x5F6906e52918cac7c2f428441b8B92C65faAd534",
-    poolFactory: "0x8DecD86959b87c64Be4978029a27D19c3e5d49DE",
+    poolFactory: "0x16697a88f99Bf9F06938e25D3343fF2144B8c3c6",
   },
   educhainTestnet: {
     gasPrice: undefined,
@@ -53,7 +59,7 @@ const networks = {
     usdc: "0x9Fa2f0872498f56Fec437d92D66842f162c6B922",
     dai: "0xAE82F939962BE8119f29E3a657fF6A07745fd735",
     weth: "0x35913884404941d616Ba76a90702572236DaF317",
-    poolFactory: "0x16697a88f99Bf9F06938e25D3343fF2144B8c3c6",
+    poolFactory: "0x8DecD86959b87c64Be4978029a27D19c3e5d49DE",
   },
 };
 
--- v3-fork/tasks/swap.js
+++ v3-fork/tasks/swap.js
@@ -1,23 +1,63 @@
 const { networks } = require("../networks");
 
-task("create-pool", "Create a pool").setAction(async (taskArgs) => {
+task("swap", "Create a pool").setAction(async (taskArgs) => {
   const { ethers } = hre;
   const [signer] = await ethers.getSigners();
 
-  // CHANGE THIS
-  const poolAddress = "";
-  const recipient = "";
-  const zeroForOne = "";
-  const amountSpecified = "";
-  const sqrtPriceLimitX96 = "";
+  // // CHANGE THIS
+  // const poolAddress = "";
+  // const recipient = "";
+  // const zeroForOne = "";
+  // const amountSpecified = "";
+  // const sqrtPriceLimitX96 = "";
 
-  const core = new ethers.Contract(poolAddress, [], signer);
-  const response = await core.swap(
-    recipient,
-    zeroForOne,
-    amountSpecified,
-    sqrtPriceLimitX96
-  );
-  const receipt = await response.wait();
-  console.log(receipt);
+  // const core = new ethers.Contract(
+  //   poolAddress,
+  //   [
+  //     {
+  //       constant: false,
+  //       inputs: [
+  //         {
+  //           name: "recipient",
+  //           type: "address",
+  //         },
+  //         {
+  //           name: "zeroForOne",
+  //           type: "bool",
+  //         },
+  //         {
+  //           name: "amountSpecified",
+  //           type: "int256",
+  //         },
+  //         {
+  //           name: "sqrtPriceLimitX96",
+  //           type: "uint160",
+  //         },
+  //       ],
+  //       name: "swap",
+  //       outputs: [
+  //         {
+  //           name: "amount0",
+  //           type: "int256",
+  //         },
+  //         {
+  //           name: "amount1",
+  //           type: "int256",
+  //         },
+  //       ],
+  //       payable: false,
+  //       stateMutability: "nonpayable",
+  //       type: "function",
+  //     },
+  //   ],
+  //   signer
+  // );
+  // const response = await core.swap(
+  //   recipient,
+  //   zeroForOne,
+  //   amountSpecified,
+  //   sqrtPriceLimitX96
+  // );
+  // const receipt = await response.wait();
+  // console.log(receipt);
 });
